e95a81e450fd3360f94dab2490b241e122cb1716
krishkdba <89520487+krishkdba@users.noreply.github.com>
2023-04-04 01:13:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello World to the Jenkins. -- version 1");
+        System.out.println("Hello World to the Jenkins. -- version 2");
     }
 }
