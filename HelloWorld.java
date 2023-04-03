2100a5918ff92dfbcc0773bae79cb71c2431f728
Karthick Nainiappan <karthickn@in.ibm.com>
2023-04-04 01:36:28 +0530

    change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
         System.out.println("Hello World to the Jenkins. -- version 3");
-=======
-        System.out.println("Hello World to the Jenkins. -- version 2");
->>>>>>> e95a81e450fd3360f94dab2490b241e122cb1716
+
     }
 }
